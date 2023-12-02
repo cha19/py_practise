cd5b4d65c0944b789d8fc64122f74b703329c5e3
charan <saicharan.gangili@gmail.com>
2023-12-02 14:31:14 -0600

    barcode scanner using raspi

---
+++ barcode/webcam_scanner.py
@@ -0,0 +1,13 @@
+import cv2
+import pyzbar
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+
+while True:
+    _, frame = cap.read()
+
+    decodedObjects = pyzbar.decode(frame)
+    for obj in decodedObjects:
+        #print("Data", obj.data)
+        cv2.putText(frame, str(obj.data), (50, 50), font, 2,
+                    (255, 0, 0), 3)
